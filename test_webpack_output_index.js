cd4f94b1f3161de5d2addfa937d04647c7c3141d
Aadi <aadi@gitlab.clearconverse.com>
2021-03-07 03:17:09 +0530

    Lint changes

--- test_webpack_output_index.js
+++ test_webpack_output_index.js
@@ -1,8 +1,8 @@
 // This is to test index.js bundle file generated using webpack
 // one can run this by 'node ./test_webpack_output_index.js'
 
-const index =  require('./dist/index.js');
+const index = require('./dist/index.js');
 (async() => {
- const response = await index.handler();
- console.log('Response is: ', JSON.stringify(response));
+  const response = await index.handler();
+  console.log('Response is: ', JSON.stringify(response));
 })();
